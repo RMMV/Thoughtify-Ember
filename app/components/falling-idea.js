9c36f935a616e0804554875141468e9862c6c50a
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-03-20 21:27:01 -0400

    Getting the idea to translate up and down.

--- app/components/falling-idea.js
+++ app/components/falling-idea.js
@@ -4,4 +4,11 @@ import layout from '../templates/components/falling-idea';
 export default Ember.Component.extend({
 	layout: layout,
 	classNames: ['falling-idea'],
+	classNameBindings: ['falling'],
+	falling: false,
+	actions: {
+		toggleFalling: function(){
+			this.set('falling', !this.get('falling'));
+		}
+	}
 });
