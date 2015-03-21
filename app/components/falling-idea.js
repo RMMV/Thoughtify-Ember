fe36e791b9a16de3481aa0f6715bdd383633028e
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-03-20 21:04:16 -0400

    Setting up the project to do styling and creating the falling-idea component.

---
+++ app/components/falling-idea.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/falling-idea';
+
+export default Ember.Component.extend({
+	layout: layout,
+	classNames: ['falling-idea'],
+});
