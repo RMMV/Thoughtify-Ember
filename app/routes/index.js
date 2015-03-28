332cedbb051d51d0e2afd18b600013246df06027
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-03-28 00:34:04 -0400

    People should be able to view the landing page regardless of if they're logged in.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	beforeModel: function(){
-		let session = this.get('session');
-		if (session.isAuthenticated) {
-			this.transitionTo('ideas');
-		}
-	}
 });
