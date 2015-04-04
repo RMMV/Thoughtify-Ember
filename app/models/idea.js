7e4d0d21160f545798b498ad1b5f0d38f253ef17
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-04-04 19:32:34 -0400

    Getting updatedAt displaying on the idea cards.

--- app/models/idea.js
+++ app/models/idea.js
@@ -2,9 +2,8 @@ import DS from 'ember-data';
 
 var Idea = DS.Model.extend({
 	body: DS.attr('string'),
-	upvotes: DS.attr('number'),
-	downvotes: DS.attr('number'),
 	title: DS.attr('string'),
+	updatedAt: DS.attr('date'),
 	image: DS.attr('string', { defaultValue: "http://placehold.it/400x500"})
 });
 
