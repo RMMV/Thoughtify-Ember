1f6ca54b6ec63a67cf7630489f50b2dde6af5cf7
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-04-04 20:29:46 -0400

    Getting the progres-bars some random data.

--- app/components/idea-card.js
+++ app/components/idea-card.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	attributeBindings: ['data-component'],
-	'data-component': 'idea-card'
+	'data-component': 'idea-card',
+	progress: Ember.computed(function(){
+		return Math.floor(Math.random()*100) + '%';
+	})
 });
